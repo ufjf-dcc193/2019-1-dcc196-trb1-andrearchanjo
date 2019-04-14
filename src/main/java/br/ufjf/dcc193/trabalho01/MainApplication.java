8d57b79b4b792b7d3b8ef7a7d021104189c5da1a
André Archanjo <archanjoandre@yahoo.com.br>
2019-04-14 18:08:01 -0300

    commit inicial

---
+++ src/main/java/br/ufjf/dcc193/trabalho01/MainApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trabalho01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
